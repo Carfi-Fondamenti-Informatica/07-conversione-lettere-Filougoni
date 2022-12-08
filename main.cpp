8491c7b4f11bdcb0a56ef34f14d468fb203d4914
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-12-08 13:04:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
-int main(){
+#include "lib.h"
+using namespace std;
+int main() {
+    char c;
+    cin >> c;
+if(conversione(c)==true){
+    cout<< c <<endl;
+}
+else{
+    cout<<"errore"<<endl;
+}
   return 0;
 }
